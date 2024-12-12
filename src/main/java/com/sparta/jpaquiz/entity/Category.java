34b545be4d9d1d3d820fba429408bb827076b630
kanghuijin <rkd8527@naver.com>
2024-12-12 17:23:20 +0900

    feat: 퀴즈 제출합니다!

--- src/main/java/com/sparta/jpaquiz/entity/Category.java
+++ src/main/java/com/sparta/jpaquiz/entity/Category.java
@@ -1,12 +1,7 @@
 package com.sparta.jpaquiz.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +9,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Category {
 
     @Id
@@ -28,7 +24,15 @@ public class Category {
      * 조건: 연관된 책(Book) 엔티티는 실제로 필요할때만 DB에서 조회하도록 명시적으로 설정
      * Hint: Cascade 설정을 추가하고 Lazy 로딩 전략을 사용하세요!
      */
-    @OneToMany(...)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Book> books = new ArrayList<>();
 
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+        book.setCategory(this);
+    }
 }
